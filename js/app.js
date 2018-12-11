d4bd8c83eb4e5b7fdb3e29cab7efc0c56783a337
Chintan Gandhi <cgandhi4@gmail.com>
2018-12-11 23:34:48 +0400

    Modified JS

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+/*jshint esversion: 6 */
 // Initailizing variable to enter interested location (Make reference to the geolocation utility tool)
+"use strict";
 var locationMap = {
     center: {
         lat: 17.686816,
@@ -13,65 +15,21 @@ var markers = [];
 // var check = [1,2,3];
 
 //(Check if this requires to be hidden)
-CLIENT_ID = 'V4EOEMQUHFKPCJPPP41L412QV52QWXXINHYSPN3MWYEKLAII';
-CLIENT_SECRET = 'MZ1PW0XKIQSWLH2T1WKIQOIKRH45CEDTO1D5E51VPKG0BBDL';
-V = '20181126';
-
-// Listing locations with titles and geocodes
-var locations = [{
-        title: 'Green Park Hotel Visakhapatnam',
-        // location:{lat:17.713882, lng:83.305155} 
-        location: {
-            lat: 17.715709215080107,
-            lng: 83.30631487005911
-        }
-    },
-    {
-        title: 'VB',
-        location: {
-            lat: 17.7155120,
-            lng: 83.3121050
-        }
-    },
-
-    {
-        title: 'IIM(Vizag)',
-        location: {
-            lat: 17.723047,
-            lng: 83.326168
-        }
-    },
-
-    {
-        title: 'Inox',
-        location: {
-            lat: 17.711196938507506,
-            lng: 83.31579141855502
-        }
-    },
-
-    {
-        title: 'Chandu Sweets',
-        location: {
-            lat: 17.724917,
-            lng: 83.306217
-        }
-    },
-
-];
-
+const CLIENT_ID = 'V4EOEMQUHFKPCJPPP41L412QV52QWXXINHYSPN3MWYEKLAII';
+const CLIENT_SECRET = 'MZ1PW0XKIQSWLH2T1WKIQOIKRH45CEDTO1D5E51VPKG0BBDL';
+const V = '20181126';
+var map;
+let displayWindow;
 function initMap() {
     // Building new map
-    map = new google.maps.Map(document.getElementById('map'), locationMap);
-
+    map = new google.maps.Map(document.getElementsByClassName('map'), locationMap);
     // Create instances from classes in google maps api
     var bounds = new google.maps.LatLngBounds();
-    var displayWindow = new google.maps.InfoWindow();
-
+    displayWindow = new google.maps.InfoWindow();
     // Adding markers on the map with the locations
-    for (var i = 0; i < locations.length; i++) {
-        var position = locations[i].location
-        var title = locations[i].title
+    for(let i = 0; i < locations.length; i++) {
+        var position = locations[i].location;
+        var title = locations[i].title;
         var marker = new google.maps.Marker({
             map: map,
             position: position,
@@ -80,13 +38,14 @@ function initMap() {
             id: i
         });
 
-        // A location maker created is added to a collection of markers        
+        // A location maker created is added to a collection of markers
         markers.push(marker);
         // An event listener that responds with animation and a display window when the marker on the map is clicked
-        marker.addListener('click', function () {
+        marker.addListener('click', function() {
             populateDisplayWindow(this, displayWindow);
             stopAnimation();
             this.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout( () => this.setAnimation(null), 2000);
         });
         // (Extends map to include all markers)
         bounds.extend(markers[i].position);
@@ -95,42 +54,45 @@ function initMap() {
     // (Extends map to include all markers)
     map.fitBounds(bounds);
 
-};
+}
 
-// The viewmodel to connect view, what we see on the webpage 
+function mapError(){
+    window.alert("There is an error loading the map");
+}
+
+// The viewmodel to connect view, what we see on the webpage
 // to the model, data stored in the database or in the array for this
-// case 
-
+// case
 var LocationsViewModel = function () {
     var self = this;
 
     // Connecting an element in HTML to model, in this case an array.
     this.locationList = ko.observableArray([]);
     this.query = ko.observable('');
+    
 
-    //Initialization to assign view element with model data
-    var locationA = function (data) {
-        this.title = ko.observable(data.title);
-    }
+    // //Initialization to assign view element with model data
+    // var locationA = function (data) {
+    //     this.title = ko.observable(data.title);
+    // };
 
-    //Creating view list from model array by creating elements 
+    //Creating view list from model array by creating elements
     //and assigning it with object-literal values from the model data
-    locations.forEach(function (locationSingle) {
-        self.locationList.push(new locationA(locationSingle));
+    locations.forEach(function(locationSingle){
+        self.locationList.push(locationSingle);
     });
     // When clicked on a list item the corresponding marker is seen on the map
     self.markerAlert = function (selectedLoc) {
         // Stops the animation when another click event occurs.
         stopAnimation();
-        // The clicked list item marker animates on the map 
+        // The clicked list item marker animates on the map
         for (var selectMarker in markers) {
-            if (markers[selectMarker].title === selectedLoc.title()) {
+            if (markers[selectMarker].title === selectedLoc.title) {
                 markers[selectMarker].setAnimation(google.maps.Animation.BOUNCE);
-                // Create new instance of infowindow
-                var displayWindowlist = new google.maps.InfoWindow();
-                populateDisplayWindow(markers[selectMarker], displayWindowlist);
-            };
-        };
+                // Create new instance of infowindow                
+                populateDisplayWindow(markers[selectMarker], displayWindow);
+            }
+        }
 
     };
 
@@ -144,12 +106,12 @@ var LocationsViewModel = function () {
         } else {
             // All markers are initially hidden
             hideListings();
-            // Depending on what is entered the corressponding matches are shown in the list        
-            locationFiltered = self.locationList().filter(locationA => locationA.title().toLowerCase().indexOf(self.query().toLowerCase()) > -1);
-            // Adding markers for those locations that are seen in the list           
+            // Depending on what is entered the corressponding matches are shown in the list
+            var locationFiltered = self.locationList().filter(location => location.title.toLowerCase().indexOf(self.query().toLowerCase()) > -1);
+            // Adding markers for those locations that are seen in the list
             for (var singleMarker in markers) {
                 for (var singleLocation in locationFiltered) {
-                    if (markers[singleMarker].title === locationFiltered[singleLocation].title()) {
+                    if (markers[singleMarker].title === locationFiltered[singleLocation].title) {
                         // Over here markers are seen based on which locations are filtered
                         markers[singleMarker].setMap(map);
                     }
@@ -157,14 +119,14 @@ var LocationsViewModel = function () {
             }
             // The filtered list based on the query information is returned
             return locationFiltered;
-        };
+        }
 
 
     });
 
 };
 
-//Activatation to connect view to model 
+//Activatation to connect view to model
 ko.applyBindings(new LocationsViewModel());
 
 //Displays all markers in th locations array on the map
@@ -174,14 +136,14 @@ function showListings() {
         markers[i].setMap(map);
         // bounds.extend(markers[i].position)
     }
-};
+}
 
 //Hides all markers in the locations array from the map
 function hideListings() {
     for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
     }
-};
+}
 
 //Stops animation of all markers displayed on the map
 function stopAnimation() {
@@ -189,21 +151,21 @@ function stopAnimation() {
         markers[i].setAnimation(null);
     }
 
-};
+}
 
 //Displays information in the infowindow of the marker
 function populateDisplayWindow(marker, infowindow) {
     if (infowindow.marker != marker) {
         infowindow.setContent('');
         infowindow.marker = marker;
-        // Initialization to access endpoints of location from the Foursquare API.           
+        // Initialization to access endpoints of location from the Foursquare API.
         var addressURL = 'https://api.foursquare.com/v2/venues/search?ll=' + marker.position.lat() +
             ',' + marker.position.lng() + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + "&v=" + V;
         // Obtain address of the location from the Foursquare API and placing in the marker infowindow display
         $.getJSON(addressURL, function (data) {
-            locationAddress = data.response.venues[0].location.formattedAddress;
+            let locationAddress = data.response.venues[0].location.formattedAddress;
             infowindow.setContent('<div>' + marker.title + '</div>' + '<div>' + locationAddress + '</div>');
-            //For a error response recieved from the FourSquare API a messeage is displayed in the infowindow                      
+        //For a error response recieved from the FourSquare API a messeage is displayed in the infowindow
         }).error(function (e) {
             infowindow.setContent('<div>There was an error calling the API</div>');
         });
@@ -213,18 +175,18 @@ function populateDisplayWindow(marker, infowindow) {
         });
     }
 
-};
+}
 
 //Toggles the viewlist when the navigation button is clicked
 function toggle() {
     var nav = document.getElementById('nav');
     var optionsBox = document.getElementById('options-box');
-    // var $mapdisplay = $("body .container #map");   
+    // var $mapdisplay = $("body .container #map");
     if (optionsBox.style.display === "none") {
         optionsBox.style.display = "block";
         nav.style.left = "330px";
     } else {
         optionsBox.style.display = "none";
         nav.style.left = "1px";
-    };
-};
+    }
+}
---
+++ js/data.js
@@ -0,0 +1,39 @@
+// Listing locations with titles and geocodes
+var locations = [{
+        title: 'Green Park Hotel Visakhapatnam',        location: {
+            lat: 17.715709215080107,
+            lng: 83.30631487005911
+        }
+    },
+    {
+        title: 'VB',
+        location: {
+            lat: 17.7155120,
+            lng: 83.3121050
+        }
+    },
+
+    {
+        title: 'IIM(Vizag)',
+        location: {
+            lat: 17.723047,
+            lng: 83.326168
+        }
+    },
+
+    {
+        title: 'Inox',
+        location: {
+            lat: 17.711196938507506,
+            lng: 83.31579141855502
+        }
+    },
+
+    {
+        title: 'Chandu Sweets',
+        location: {
+            lat: 17.724917,
+            lng: 83.306217
+        }
+    },
+]; 
